37d4cce292b8c67efae8841b1dbc12b43dea536c
hoemoon <hoemoon.song@gmail.com>
2018-09-05 16:11:09 +0900

    Add new target

---
+++ LightMac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LightMac
+//
+//  Created by hoemoon on 05/09/2018.
+//  Copyright © 2018 Piano. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
