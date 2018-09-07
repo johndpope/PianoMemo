c1ce64aaca872d87e642f1263153fc25e1309ccd
hoemoon <hoemoon.song@gmail.com>
2018-09-07 14:56:16 +0900

    Add menu event to collectionView

--- LightMac/MainNSViewController.swift
+++ LightMac/MainNSViewController.swift
@@ -9,35 +9,25 @@
 import Cocoa
 
 class MainNSViewController: NSViewController {
-    @IBOutlet weak var collectionView: NSCollectionView!
+    @IBOutlet weak var collectionView: MainCollectionView!
     weak var persistentContainer: NSPersistentContainer!
 
-    var dummy = [Note]()
+    @objc let managedContext: NSManagedObjectContext
 
-    lazy var mainContext: NSManagedObjectContext = {
-        let context = persistentContainer.viewContext
-        context.automaticallyMergesChangesFromParent = true
-        return context
-    }()
+    required init?(coder: NSCoder) {
+        managedContext = (NSApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        super.init(coder: coder)
+    }
 
     lazy var backgroundContext: NSManagedObjectContext = {
         return persistentContainer.newBackgroundContext()
     }()
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        persistentContainer = (NSApplication.shared.delegate as! AppDelegate).persistentContainer
-    }
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        collectionView.dataSource = self
-
-        for _ in 1...5 {
-            let note = Note(context: mainContext)
-            note.content = "Curabitur blandit tempus porttitor."
-            dummy.append(note)
-        }
+        let cell = NSNib(nibNamed: NSNib.Name(rawValue: "NoteCell"), bundle: nil)
+        collectionView.register(cell, forItemWithIdentifier: NSUserInterfaceItemIdentifier(rawValue: "NoteCell"))
+        collectionView.delegate = self
     }
 
     override var representedObject: Any? {
@@ -48,16 +38,36 @@ class MainNSViewController: NSViewController {
 
 }
 
-extension MainNSViewController: NSCollectionViewDataSource {
-    func collectionView(_ collectionView: NSCollectionView, numberOfItemsInSection section: Int) -> Int {
-        return dummy.count
+extension MainNSViewController: NSCollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: NSCollectionView, layout collectionViewLayout: NSCollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> NSSize {
+        return NSSize(width: 200, height: 300)
     }
 
-    func collectionView(_ collectionView: NSCollectionView, itemForRepresentedObjectAt indexPath: IndexPath) -> NSCollectionViewItem {
-        if let cell = collectionView.makeItem(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "NoteCell"), for: indexPath) as? NoteCell {
-            cell.note = dummy[indexPath.item]
-            return cell
+    func collectionView(_ collectionView: NSCollectionView, didSelectItemsAt indexPaths: Set<IndexPath>) {
+        
+    }
+}
+
+extension MainNSViewController {
+    func saveIfneed() {
+        if !managedContext.commitEditing() {
+            NSLog("\(NSStringFromClass(type(of: self))) unable to commit editing before saving")
         }
-        return NSCollectionViewItem()
+        if managedContext.hasChanges {
+            do {
+                try managedContext.save()
+            } catch {
+                let nserror = error as NSError
+                NSApplication.shared.presentError(nserror)
+            }
+        }
+    }
+
+    private func setupDummy() {
+        for _ in 1...5 {
+            let note = Note(context: managedContext)
+            note.content = "Curabitur blandit tempus porttitor."
+        }
+        saveIfneed()
     }
 }
--- LightMac/NoteCell.swift
+++ LightMac/NoteCell.swift
@@ -2,24 +2,18 @@
 //  NoteCell.swift
 //  LightMac
 //
-//  Created by hoemoon on 06/09/2018.
+//  Created by hoemoon on 07/09/2018.
 //  Copyright © 2018 Piano. All rights reserved.
 //
 
 import Cocoa
 
 class NoteCell: NSCollectionViewItem {
-    var note: Note? {
-        didSet {
-            if let note = note, let content = note.content {
-                textField?.stringValue = content
-            }
-        }
-    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Do view setup here.
         view.wantsLayer = true
-        view.layer?.backgroundColor = NSColor.lightGray.cgColor
+        view.layer?.backgroundColor = NSColor.white.cgColor
     }
 }
