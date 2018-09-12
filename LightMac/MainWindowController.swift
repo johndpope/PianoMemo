973ef018496f748192bfcd4970cc41a191e00bcc
hoemoon <hoemoon.song@gmail.com>
2018-09-12 15:57:08 +0900

    Add resize window.

--- LightMac/MainWindowController.swift
+++ LightMac/MainWindowController.swift
@@ -9,13 +9,48 @@
 import AppKit
 
 class MainWindowController: NSWindowController {
-    override func windowWillLoad() {
-        super.windowWillLoad()
+    private let inputTextViewHeight: CGFloat = 50
+    private let maxCellCount: CGFloat = 9
+    private let width: CGFloat = 550
+    private let cellHeight: CGFloat = 50
+    private var topLeftCorner: CGPoint!
+
+    private var defaultSize: NSSize {
+        return NSSize(
+            width: width,
+            height: maxCellCount * cellHeight + inputTextViewHeight
+        )
     }
 
     override func windowDidLoad() {
         super.windowDidLoad()
-        window?.setContentSize(NSSize(width: 500, height: 500))
-        window?.center()
+        guard let window = window else { return }
+        window.setContentSize(defaultSize)
+        window.center()
+
+        topLeftCorner = CGPoint(
+            x: window.frame.origin.x, y: window.frame.origin.y + window.frame.height)
+        if let mainViewController = contentViewController as? MainNSViewController {
+            mainViewController.delegate = self
+        }
+    }
+}
+
+protocol WindowResizeDelegate: class {
+    var heightOfRow: CGFloat { get }
+    func setWindowHeight(with cellCount: Int)
+}
+
+extension MainWindowController: WindowResizeDelegate {
+    var heightOfRow: CGFloat {
+        return self.cellHeight
+    }
+
+    func setWindowHeight(with cellCount: Int) {
+        let count = min(CGFloat(cellCount), maxCellCount)
+        let newSize = NSSize(width: width,height: cellHeight * count + inputTextViewHeight)
+        print(topLeftCorner.y, "topLeft")
+        window?.setContentSize(newSize)
+        window?.setFrameTopLeftPoint(topLeftCorner!)
     }
 }
