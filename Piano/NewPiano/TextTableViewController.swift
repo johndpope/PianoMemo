9bf09e252b43c2165020a8e5ff54cd04e00bf2fa
Kevin <Kevin_baba@naver.com>
2018-11-07 17:36:44 +0900

    improvement tab bar architecture

---
+++ Piano/NewPiano/TextTableViewController.swift
@@ -0,0 +1,46 @@
+//
+//  TextTableViewController.swift
+//  Piano
+//
+//  Created by Kevin Kim on 07/11/2018.
+//  Copyright © 2018 Piano. All rights reserved.
+//
+
+import UIKit
+
+class TextTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    
+    @IBAction func tapFastNote(_ sender: Any) {
+        
+        
+    }
+    
+
+}
+
+extension TextTableViewController: UITextViewDelegate {
+    
+}
+
+//extension TextTableViewController: UITableViewDataSource {
+//
+//}
+//
+//extension TextTableViewController: UITableViewDelegate {
+//
+//}
+//
+//extension TextTableViewController: UICollectionViewDataSource {
+//
+//}
+//
+//extension TextTableViewController: UICollectionViewDelegate {
+//
+//}
