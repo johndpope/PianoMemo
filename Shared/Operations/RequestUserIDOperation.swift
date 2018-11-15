41a4cbe31453078f85cd0b56bf737442964df690
hoemoon <hoemoon.song@gmail.com>
2018-11-15 12:34:14 +0900

    Add Branch sdk

--- Shared/Operations/RequestUserIDOperation.swift
+++ Shared/Operations/RequestUserIDOperation.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import CloudKit
+import Branch
 
 class RequestUserIDOperation: AsyncOperation {
     private let container: CKContainer
@@ -19,22 +20,38 @@ class RequestUserIDOperation: AsyncOperation {
     override func main() {
         guard UserDefaults.getUserIdentity() == nil else {
             state = .Finished
+            setBranch(UserDefaults.getUserIdentity())
             return
         }
-        container.fetchUserRecordID { [weak self] recordID, error in
-            guard let recordID = recordID else {
-                self?.state = .Finished
-                return
-            }
-            self?.container.discoverUserIdentity(withUserRecordID: recordID) {
-                identity, error in
-                if error == nil {
-                    UserDefaults.setUserIdentity(identity: identity)
-                } else {
-                    print(error!)
+        if let id = UserDefaults.getUserIdentity(),
+            let recordName = id.userRecordID?.recordName {
+            Branch.getInstance()?.setIdentity(recordName)
+            state = .Finished
+            return
+        } else {
+            container.fetchUserRecordID { [weak self] recordID, error in
+                guard let recordID = recordID else {
+                    self?.state = .Finished
+                    return
                 }
-                self?.state = .Finished
+                self?.container.discoverUserIdentity(withUserRecordID: recordID) {
+                    identity, error in
+                    if error == nil {
+                        UserDefaults.setUserIdentity(identity: identity)
+                        self?.setBranch(identity)
+                    } else {
+                        print(error!)
+                    }
+                    self?.state = .Finished
+                }
             }
         }
+    }
+
+    private func setBranch(_ identity: CKUserIdentity?) {
+        guard let identity = identity,
+            let recordName = identity.userRecordID?.recordName else { return }
+
+        Branch.getInstance()?.setIdentity(recordName)
     }
 }
--- Shared/RemoteStorageService.swift
+++ Shared/RemoteStorageService.swift
@@ -77,15 +77,14 @@ class RemoteStorageSerevice {
             let completionOperation = BlockOperation(block: completion)
             let delayed = BlockOperation { [weak self] in
                 guard let self = self else { return }
-                self.syncController.processDelayedTasks()
+                if database == self.privateDatabase {
+                    self.syncController.processDelayedTasks()
+                }
             }
             fetchZoneChange.addDependency(fetchDatabaseChange)
             handlerZoneChange.addDependency(fetchZoneChange)
             completionOperation.addDependency(handlerZoneChange)
-
-            if database == privateDatabase {
-                delayed.addDependency(completionOperation)
-            }
+            delayed.addDependency(completionOperation)
 
             self.privateQueue.addOperations(
                 [fetchDatabaseChange, fetchZoneChange, handlerZoneChange, completionOperation, delayed],
