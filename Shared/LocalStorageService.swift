62941f978d0136048a4068f06e506ebab4b23af1
Kevin <Kevin_baba@naver.com>
2018-12-06 23:08:07 +0900

    change checklist emoji for lower version

--- Shared/LocalStorageService.swift
+++ Shared/LocalStorageService.swift
@@ -39,7 +39,7 @@ class LocalStorageService: NSObject {
 
     private lazy var reachability = Reachability()
 
-    public lazy var persistentContainer: NSPersistentContainer = {
+    public var persistentContainer: NSPersistentContainer = {
         let container = NSPersistentContainer(name: "Light")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
@@ -248,11 +248,11 @@ extension LocalStorageService {
         do {
             let count = try backgroundContext.count(for: LocalStorageService.allfetchRequest())
             if count == 0 {
-                createLocally(string: "1. The quickest way to copy the text\n✷ slide texts to the left side to copy them\n✷ Tap Select on the upper right, and you can copy the text you like.\n✷ Click “Convert” on the bottom right to send the memo as Clipboard, image or PDF.\n✷ Go to “How to use” in Navigate to see further information.".loc, tags: "")
-                createLocally(string: "2. How to tag with Memo\n✷ On any memo, tap and hold the tag to paste it into the memo you want to tag with.\n✷ If you'd like to un-tag it, paste the same tag back into the memo.\n✷ Go to “How to use” in Setting to see further information.".loc, tags: "")
-                createLocally(string: "3. How to highlight\n✷ Click the ‘Highlighter’ button below.\n✷ Slide the texts you want to highlight from left to right.\n✷ When you slide from right to left, the highlight will be gone.\n✷ Go to “How to use” in Setting to see further information.".loc, tags: "")
-                createLocally(string: "4. How to use Emoji List\n✷ Use the shortcut keys (-,* etc), and put a space to make it list.\n✷ Both shortcut keys and emoji can be modified in the Customized List of the settings.".loc, tags: "")
-                createLocally(string: "5. How to add the schedules\n✷ Write down the time/details to add your schedules.\n✷ Ex: Meeting with Cocoa at 3 pm\n✷ When you write something after using shortcut keys and putting a spacing, you can also add it on reminder.\n✷ Ex: -To buy iPhone charger.".loc, tags: "") { [weak self](_) in
+                createLocally(string: "1. The quickest way to copy the text\n♩ slide texts to the left side to copy them\n✷ Tap Select on the upper right, and you can copy the text you like.\n✷ Click “Convert” on the bottom right to send the memo as Clipboard, image or PDF.\n✷ Go to “How to use” in Navigate to see further information.".loc, tags: "")
+                createLocally(string: "2. How to tag with Memo\n♩ On any memo, tap and hold the tag to paste it into the memo you want to tag with.\n✷ If you'd like to un-tag it, paste the same tag back into the memo.\n✷ Go to “How to use” in Setting to see further information.".loc, tags: "")
+                createLocally(string: "3. How to highlight\n♩ Click the ‘Highlighter’ button below.\n✷ Slide the texts you want to highlight from left to right.\n✷ When you slide from right to left, the highlight will be gone.\n✷ Go to “How to use” in Setting to see further information.".loc, tags: "")
+                createLocally(string: "4. How to use Emoji List\n♩ Use the shortcut keys (-,* etc), and put a space to make it list.\n✷ Both shortcut keys and emoji can be modified in the Customized List of the settings.".loc, tags: "")
+                createLocally(string: "5. How to add the schedules\n♩ Write down the time/details to add your schedules.\n✷ Ex: Meeting with Cocoa at 3 pm\n✷ When you write something after using shortcut keys and putting a spacing, you can also add it on reminder.\n✷ Ex: -To buy iPhone charger.".loc, tags: "") { [weak self](_) in
                     guard let self = self else { return }
                     self.keyValueStore.set(true, forKey: "didAddTutorials")
                 }
