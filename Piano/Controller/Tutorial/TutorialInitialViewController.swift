8d5f883ce752ad8f6096b0df8d889e1e5326a398
joohyuck <joohyuck2@gmail.com>
2019-02-01 20:10:53 +0900

    initialize tutorial screen animation

--- Piano/Controller/Tutorial/TutorialInitialViewController.swift
+++ Piano/Controller/Tutorial/TutorialInitialViewController.swift
@@ -9,25 +9,62 @@
 import UIKit
 
 class TutorialInitialViewController: UIViewController {
-
+    
+    @IBOutlet weak var header: UILabel!
+    @IBOutlet weak var divider: UIView!
+    @IBOutlet weak var subHeader: UILabel!
+    
     @IBOutlet weak var note0: UIView!
     @IBOutlet weak var note1: UIView!
     @IBOutlet weak var note2: UIView!
     
+    @IBOutlet weak var nextBtn: UIButton!
+    
+    var headerArray: [UIView] = []
     var noteArray: [UIView] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        headerArray = [header, divider, subHeader]
         noteArray = [note0, note1, note2]
+        
+        for view in headerArray {
+            view.layer.opacity = 0
+        }
+        
         for note in noteArray {
             note.layer.opacity = 0
-            note.layer.shadowColor = UIColor.gray.cgColor
-            note.layer.shadowOffset = CGSize(width: 4, height: 4)
+            note.layer.masksToBounds = false
+            note.layer.shadowColor = UIColor.lightGray.cgColor
+            note.layer.shadowOffset = CGSize(width: 0, height: 2)
+            //note.layer.shadowRadius = 10
+            note.layer.shadowOpacity = 0.2
         }
+        
+        nextBtn.layer.opacity = 0
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+    
+        queueNoteAnimation()
+        Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false) { _ in
+            self.queueHeaderAnimation()
+            self.queueButtonAnimation()
+        }
+    }
+    
+    func queueHeaderAnimation() {
+        for (index, view) in headerArray.enumerated() {
+            let interval = 0.3 * Double(index)
+            UIView.animate(withDuration: 0.8, delay: interval, options: .curveEaseIn, animations: {
+                view.layer.opacity = 1
+            })
+        }
+    }
+    
+    func queueNoteAnimation() {
         for (index, note) in noteArray.enumerated() {
             let interval = 0.2 * Double(index)
             note.center.y += 30
@@ -36,6 +73,12 @@ class TutorialInitialViewController: UIViewController {
                 note.layer.opacity = 1
             })
         }
+    }
+    
+    func queueButtonAnimation() {
+        UIView.animate(withDuration: 0.8, delay: 0, options: .curveEaseIn, animations: {
+            self.nextBtn.layer.opacity = 1
+        })
     }
     
     override var prefersStatusBarHidden: Bool {
