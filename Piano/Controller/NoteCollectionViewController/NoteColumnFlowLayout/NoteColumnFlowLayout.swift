96cea1574b3aa79059353460249a56b09654acca
Kevin <Kevin_baba@naver.com>
2019-02-01 20:34:59 +0900

    add write button and improve BlockTableView performance

--- Piano/Controller/NoteCollectionViewController/NoteColumnFlowLayout/NoteColumnFlowLayout.swift
+++ Piano/Controller/NoteCollectionViewController/NoteColumnFlowLayout/NoteColumnFlowLayout.swift
@@ -20,12 +20,13 @@ class NoteColumnFlowLayout: UICollectionViewFlowLayout {
         let maxNumColumns = Int(availableWidth / minColumnWidth)
         let cellWidth = (availableWidth / CGFloat(maxNumColumns)).rounded(.down)
         //height
-        let margin: CGFloat = 8.0
+        let margin: CGFloat = 16
+        let spacing: CGFloat = 8
         let headlineHeight = NSAttributedString(string: "1234567890", attributes: [.font: Font.preferredFont(forTextStyle: .headline)]).size().height
         let subHeadHeight = NSAttributedString(string: "1234567890\n1234567890", attributes: [.font: Font.preferredFont(forTextStyle: .subheadline)]).size().height
         let dateHeight = NSAttributedString(string: "1234567890", attributes: [.font: Font.preferredFont(forTextStyle: .caption1)]).size().height
 
-        let fullHeight: CGFloat = margin * 4 + headlineHeight + subHeadHeight + dateHeight
+        let fullHeight: CGFloat = margin * 2 + spacing * 2 + headlineHeight + subHeadHeight + dateHeight
         self.itemSize = CGSize(width: cellWidth, height: fullHeight)
         self.sectionInset = UIEdgeInsets(top: self.minimumInteritemSpacing, left: 0, bottom: 0, right: 0)
         self.sectionInsetReference = .fromSafeArea
