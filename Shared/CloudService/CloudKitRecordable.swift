79cae20232ef130341cae800cd06408d023a1359
hoemoon <hoemoon.song@gmail.com>
2019-02-25 22:23:36 +0900

    Add documentation to cloudkitRecordable

--- Shared/CloudService/CloudKitRecordable.swift
+++ Shared/CloudService/CloudKitRecordable.swift
@@ -9,10 +9,7 @@
 import CloudKit
 import CoreData
 
-enum CloudKitRecodableKeys: String {
-    case recordID
-}
-
+/// 클라우드킷 레코드로 표현 가능한 객체를 정의하는 프로토콜
 protocol CloudKitRecordable: class {
     var isMine: Bool { get }
     var recordArchive: Data? { get set }
@@ -30,24 +27,29 @@ extension CloudKitRecordable {
 }
 
 extension Note: CloudKitRecordable {
+    /// 로컬에만 존재하는 키를 표현
     var localExclusiveKeys: [String] {
         return ["createdBy", "isMine", "isShared", "markedForDeletionDate", "markedForRemoteDeletion", "markedForUploadReserved", "modifiedBy", "recordArchive", "recordID"]
     }
 }
 
 extension ImageAttachment: CloudKitRecordable {
+    /// 로컬에만 존재하는 키를 표현
     var localExclusiveKeys: [String] {
         return ["isMine", "markedForDeletionDate", "markedForRemoteDeletion", "markedForUploadReserved", "recordArchive", "recordID"]
     }
 }
 
 extension Folder: CloudKitRecordable {
+    /// 로컬에만 존재하는 키를 표현
     var localExclusiveKeys: [String] {
         return ["isMine", "markedForDeletionDate", "markedForRemoteDeletion", "markedForUploadReserved", "recordArchive", "recordID"]
     }
 }
 
 extension CloudKitRecordable where Self: NSManagedObject {
+    /// 코어데이터 객체를 클라우드킷 레코드로 변환해주는 computed property
+    /// 키 각각의 타입에 따라서 다른 serialize 과정을 거칩니다.
     var cloudKitRecord: CKRecord? {
         guard let ckRecord = self.recordArchive?.ckRecorded else { return nil }
         let attrkeys = entity.attributesByName.keys.filter { !localExclusiveKeys.contains($0) }
@@ -90,6 +92,7 @@ extension CloudKitRecordable where Self: NSManagedObject {
                 }
             }
         }
+        // 관계를 가진 객체가 있는 경우
         let relationshipKeys = self.entity.relationshipsByName.keys
         for key in relationshipKeys {
             var ckReference: CKRecord.Reference?
@@ -107,6 +110,8 @@ extension CloudKitRecordable where Self: NSManagedObject {
 }
 
 extension Data {
+    /// 임시로 바이너리 데이터를 디스크에 저장한 후
+    /// 업로드시 사용할 url을 제공하는 computed property
     var temporaryURL: URL? {
         do {
             let filename = "\(ProcessInfo.processInfo.globallyUniqueString)._file.bin"
